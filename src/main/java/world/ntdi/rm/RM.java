0f90886b2bace82239180e8967660fcc8ad91d35
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-03-23 16:43:45 -0600

    updated main plugin

--- src/main/java/world/ntdi/rm/RM.java
+++ src/main/java/world/ntdi/rm/RM.java
@@ -1,17 +1,18 @@
 package world.ntdi.rm;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RM extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getLogger().info("RM is enabled!");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info("RM is disabled!");
     }
 }
